f3e398466abf5cfc8bb70c9a3272f5580c7810ae
FabrizioFornone <fabrizio.fornone@hotmail.it>
2021-12-17 10:05:18 +0100

    Correzione con Ottavio

---
+++ Palindrome/JS/script.js
@@ -0,0 +1,31 @@
+/* Chiedere all’utente di inserire una parola Creare
+ una funzione per capire se la parola inserita è palindroma */
+
+// Funzione per avere una parola invertita tramite ciclo for inverso
+
+function isPalindroma(wordToReverse) {
+  let result = "";
+  for (i = wordToReverse.length - 1; i >= 0; i--) {
+    result += wordToReverse[i];
+    console.log(result);
+  }
+
+  // If Booleano, con true esce
+  if (result === wordToReverse) {
+    return true;
+  }
+  // implicito
+  return false;
+}
+
+// Prompt
+
+const wordRequest = prompt("Inserisci una parola");
+
+// If di verifica
+
+if (isPalindroma(wordRequest)) {
+  console.log("è palindroma");
+} else {
+  console.log("NON è palindromo");
+}
